f17f17b50ae180708ada996dc48e3eea82b6adce
araraya <aryadanihlesmana@gmail.com>
2023-11-22 16:43:26 +0700

    (feature)validation form

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -11,11 +11,21 @@
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" placeholder="Title" name="title">
-              </div>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" placeholder="Title" name="title">
+                @error('title')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+                @enderror
+            </div>
             <div class="mb-3">
-              <label for="slug">Slug</label>
-              <input type="text" class="form-control" id="slug" placeholder="Slug" name="slug">
+                <label for="slug">Slug</label>
+                <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" placeholder="Slug" name="slug">
+                @error('slug')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="slug" class="form-label">Select</label>
@@ -27,6 +37,9 @@
             </div>
             <div class="mb-3">
                 <label for="body" class="form-label">Body</label>
+                @error('body')
+                <p class="text-danger">{{$message}}</p>
+                @enderror
                 <input id="body" type="hidden" name="body">
                 <trix-editor input="body"></trix-editor>
             </div>
