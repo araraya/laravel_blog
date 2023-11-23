a2bbcb3744d223df6ce77e35a039c5bcbf98f68a
araraya <aryadanihlesmana@gmail.com>
2023-11-23 17:28:01 +0700

    (feature)update all view image page

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -11,7 +11,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <h2 class="mb-3">{{$blog->title}}</h3>
+                
+                @if($blog->image)
+                <img class="img-fluid rounded" src="{{asset('storage/' . $blog->image)}}" alt="{{$blog->category->name}}">
+                @else
                 <img class="img-fluid rounded" src="https://source.unsplash.com/800x400/?{{$blog->category->name}}" alt="">
+                @endif
             <p class="mt-3 mb-2 fs-5">By <a class="text-decoration-none" href="/posts?author={{$blog->user->username}}">{{$blog->user->name}}</a> in <a class="text-decoration-none" href="/posts?category={{$blog->category->slug}}">{{$blog->category->name}}</a></p>
             
             <div class="mt-3 fs-5">
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -20,7 +20,11 @@
   </div>
 </form>
 <div class="card mb-3">
+    @if($posts[0]->image)
+    <img class="img-fluid rounded" src="{{asset('storage/' . $posts[0]->image)}}" alt="{{$posts[0]->category->name}}">
+    @else
     <img src="https://source.unsplash.com/1200x400/?{{$posts[0]->category->name}}" class="card-img-top" alt="...">
+    @endif
     <div class="card-body text-center">
       <h3 class="card-title">{{$posts[0]->title}}</h3>
 
@@ -46,7 +50,12 @@
                 <div class="position-absolute px-3 py-2" style="background-color:rgba(0, 0, 0, 0.469)">
                     <a class="text-decoration-none text-white" href="/posts?category={{$post->category->slug}}">{{$post->category->name}}</a>
                 </div>
+                
+                @if($post->image)
+                <img class="img-fluid rounded" src="{{asset('storage/' . $post->image)}}" alt="{{$post->category->name}}">
+                @else
                 <img src="https://source.unsplash.com/500x400/?{{$post->category->name}}" class="card-img-top" alt="...">
+                @endif
                 <div class="card-body text-center">
                   <h3 class="card-title">{{$post->title}}</h3>
             
