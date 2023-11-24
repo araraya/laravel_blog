e2a7f6c71e785646f20acf9607389457f741c7c5
araraya <aryadanihlesmana@gmail.com>
2023-11-24 10:26:27 +0700

    (feature)delete image when delete post

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -133,6 +133,10 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if ($post->image) {
+            Storage::delete($post->image);
+        }
+
         Post::destroy($post->id);
         return redirect('/dashboard/posts')->with('success', 'Post deleted!');
     }
